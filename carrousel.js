ec74e056a453d0cce01438c1f83e8e6505d376b3
MaxiLedesma93 <maximilianoledesma93@gmail.com>
2022-11-16 17:25:34 -0300

    carru

--- carrousel.js
+++ carrousel.js
@@ -1,6 +1,42 @@
-<script>
-    function carru(fotos[]) {
+
+let indiceCarrusel=0;
+function carruDer() {
+    let fotosurl= ["img/bff12ea928bef5c5a502f23e0ee2e6e1.jpg", "img/Fondant.jpg", 
+    "img/tortadeCasamiento.jpg", "img/Ganache.jpg"];
+    let img = document.getElementById("imgCarrusel"); 
+    if(indiceCarrusel<fotosurl.length){
+        img.src = fotosurl[indiceCarrusel];
+        indiceCarrusel++;
         
-        
+           
+    }else{      
+            indiceCarrusel=0;
+            img.src = fotosurl[indiceCarrusel];
+            indiceCarrusel++;
+               
     }
-</script>
+    
+};
+function carruIzq() {
+    let fotosurl= ["img/bff12ea928bef5c5a502f23e0ee2e6e1.jpg", "img/Fondant.jpg", 
+    "img/tortadeCasamiento.jpg", "img/Ganache.jpg"];
+   
+    let img = document.getElementById("imgCarrusel"); 
+    if(indiceCarrusel>0){
+        
+        img.src = fotosurl[indiceCarrusel];
+        indiceCarrusel--;
+        
+           
+    }else{      
+            indiceCarrusel=3; 
+            img.src = fotosurl[indiceCarrusel];
+            indiceCarrusel--;   
+    }
+    
+};
+
+
+
+
+
