a4d2206ac27a49a50db9d281b92afb1be0c60829
MaxiLedesma93 <maximilianoledesma93@gmail.com>
2022-11-21 13:46:03 -0300

    carruterminado

--- carrousel.js
+++ carrousel.js
@@ -1,39 +1,48 @@
 
 let indiceCarrusel=0;
-function carruDer() {
-    let fotosurl= ["img/bff12ea928bef5c5a502f23e0ee2e6e1.jpg", "img/Fondant.jpg", 
+window.addEventListener('load', function(){
+    let fotosurl= ["img/bff12ea928bef5c5a502f23e0ee2e6e1.jpg", "img/Fondant.jpg",
     "img/tortadeCasamiento.jpg", "img/Ganache.jpg"];
     let img = document.getElementById("imgCarrusel");
-    if(indiceCarrusel<fotosurl.length){
-        img.src = fotosurl[indiceCarrusel];
+    img.src = fotosurl[indiceCarrusel];
+    
+});
+
+    function carruDer() {
+        let fotosurl= ["img/bff12ea928bef5c5a502f23e0ee2e6e1.jpg", "img/Fondant.jpg",
+        "img/tortadeCasamiento.jpg", "img/Ganache.jpg"];
+        let img = document.getElementById("imgCarrusel");
         indiceCarrusel++;
+        if(indiceCarrusel<fotosurl.length){
+        img.src = fotosurl[indiceCarrusel];
         
-           
-    }else{      
+     
+        }else{      
             indiceCarrusel=0;
             img.src = fotosurl[indiceCarrusel];
-            indiceCarrusel++;
                
-    }
-    
-};
-function carruIzq() {
-    let fotosurl= ["img/bff12ea928bef5c5a502f23e0ee2e6e1.jpg", "img/Fondant.jpg", 
-    "img/tortadeCasamiento.jpg", "img/Ganache.jpg"];
-    let img = document.getElementById("imgCarrusel");  
-    if(indiceCarrusel>=0){
-        
+        }
+
+    };
+    function carruIzq() {
+        let fotosurl= ["img/bff12ea928bef5c5a502f23e0ee2e6e1.jpg", "img/Fondant.jpg",
+        "img/tortadeCasamiento.jpg", "img/Ganache.jpg"];
+        let img = document.getElementById("imgCarrusel");
+
         img.src = fotosurl[indiceCarrusel];
         indiceCarrusel--;
+
+        if(indiceCarrusel>=0){
         
-           
-    }else{      
+            img.src = fotosurl[indiceCarrusel];     
+        }else{      
             indiceCarrusel=fotosurl.length-1; 
             img.src = fotosurl[indiceCarrusel];
-            indiceCarrusel--;   
-    }
-    
-};
+        }
+
+    };
+
+
 
 
 
