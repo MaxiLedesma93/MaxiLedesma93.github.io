9514881ddc70f904273b509cb92789a6ebe1ffe2
MaxiLedesma93 <maximilianoledesma93@gmail.com>
2022-11-27 23:53:19 -0300

    asd
    asd

--- frm.js
+++ frm.js
@@ -4,30 +4,51 @@ function validar(){
     let msj = document.getElementById("msj");
     let listaErrores= document.getElementById("listaErrores");
     let listaEnviados= document.getElementById("listaEnviados");
-    listaErrores.innerHTML="";
-    let item= document.createElement("li");
+    nombre.classList.remove("errorCampo");
+    mail.classList.remove("errorCampo");
+    msj.classList.remove("errorCampo");
+    let item;
+    let flag= true;
     let exprReg = /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/; 
     if(nombre.value==""){
+        item= document.createElement("li");
         item.innerHTML="nombre invalido:  " + nombre.value ;
         listaErrores.appendChild(item);
-    }else if(!exprReg.test(mail.value)){        
-                item.innerHTML="mail invalido:  " + mail.value ;
-                listaErrores.appendChild(item);
-            }else if(msj.value == ""|| (msj.value.length > 200)){           
-                    item.innerHTML="mensaje invalido:  " + msj.value ;
-                    listaErrores.appendChild(item);
-                }else{
-                    listaEnviados.innerHTML="Envio correcto: ";
-                    item.innerHTML="Nombre: "+ nombre.value ;
-                    listaEnviados.appendChild(item);
-                    item= document.createElement("li");
-                    item.innerHTML="e-mail: "+ mail.value;
-                    listaEnviados.appendChild(item);
-                    item= document.createElement("li");
-                    item.innerHTML="Mensaje: "+ msj.value;
-                    listaEnviados.appendChild(item);
+        nombre.classList.add("errorCampo");
+        flag=false;
+    } 
+    if(!exprReg.test(mail.value)){   
+        item= document.createElement("li");     
+        item.innerHTML="mail invalido:  " + mail.value ;
+        listaErrores.appendChild(item);
+        mail.classList.add("errorCampo");
+        flag=false;
+    }
+    if(msj.value == ""|| (msj.value.length > 200)){   
+        item= document.createElement("li");        
+        item.innerHTML="mensaje invalido:  " + msj.value ;
+        listaErrores.appendChild(item);
+        msj.classList.add("errorCampo");
+        flag=false;
+    }
+    if(flag){
+        
+        item=document.createElement("li");
+        item.innerHTML="Envio Correcto: ";
+        listaEnviados.appendChild(item);
+        item=document.createElement("li");
+        item.innerHTML="Nombre: "+ nombre.value ;
+        listaEnviados.appendChild(item);
+        item= document.createElement("li");
+        item.innerHTML="e-mail: "+ mail.value;
+        listaEnviados.appendChild(item);
+        item= document.createElement("li");
+        item.innerHTML="Mensaje: "+ msj.value;
+        listaEnviados.appendChild(item);
+                    
+                    
 
-                }
+    }
     
     return false;
 };
